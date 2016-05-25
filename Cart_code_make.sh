a714e6e85fbc32bba4413d7e07764aa89557c0d7
Forrest Lee <forrestlee@g.ucla.edu>
2016-05-25 13:56:12 -0700

    Update Cart_code_make.sh

--- Cart_code_make.sh
+++ Cart_code_make.sh
@@ -6,4 +6,4 @@
 #  Created by Forrest Lee on 3/16/16.
 #
 gcc -o client_connect PharmaCart_Client.c
-g++ -std=c++11 -o test robot_skeleton_com_test.cpp PharmaCart_command_mgmt.cpp
+g++ -std=c++11 -o test junction_test_IR.cpp PharmaCart_command_mgmt.cpp
