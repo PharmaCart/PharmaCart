5bcdb4f3f9f0ff939655493cdf2c6081cac3fee8
Forrest Lee <forrestlee@g.ucla.edu>
2016-05-25 13:53:17 -0700

    Update Cart_code_make.sh

--- Cart_code_make.sh
+++ Cart_code_make.sh
@@ -5,5 +5,5 @@
 #
 #  Created by Forrest Lee on 3/16/16.
 #
-gcc -o client_connect parma_com/PharmaCart_Client_2.c
+gcc -o client_connect parma_com/PharmaCart_Client.c
 g++ -std=c++11 -o test parma_com/robot_skeleton_com_test.cpp parma_com/PharmaCart_command_mgmt.cpp
