20e9fedb7d3478ccd1c651b72eaf066085de4fe5
dmat219 <dmat219@gmail.com>
2016-06-15 18:16:34 -0700

    Update parmaCart.h

--- parmaCart.h
+++ parmaCart.h
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <deque>
 #include <string>
+#include <algorithm>
 #include "beacon.h"
 #include <mraa/gpio.h>
 
@@ -27,29 +28,33 @@ public:
     parmaCart(int pin);
     //objects in class
     int x,y; //cart location
+    int x2, y2; //other cart's location
+    int direction; //0: clockwise in a loop, 1: counterclockwise in a looop, 2: up at 6, 3: down at 6
     //GPIO receiver;
     mraa_gpio_context receiver;
     beacon *currentBeacon; //beacon that the cart has just passed
     beacon *nextBeacon; //next beacon on the cart's route
-    beacon *destination; //destination
+    beacon *previousBeacon; // previous beacon in order to obtain direction
+    beacon *destination; //
+    beacon *otherDestination; //other cart's destination
     deque <beacon*> route; //will store the path to destination
-    beacon *allBeacons[8]; //pointers to all beacons
+    beacon *allBeacons[7]; //pointers to all beacons
     //   deque <item> inventory;
-    char Map[11][11];
+    char Map[13][25];
     int rising_count;
     
     //functions
     bool validBeacon(int ID); //checks if the IR int recieved is valid
-    void updateCoordinates(); //call this function when a "tick" is passed
     void setCurrentBeacon(int ID); //call this function when cart passes a beacon
     void setRoute(int ID); //call this function to set route
-    void getRoute(deque <beacon*> updatedRoute); //this function will overwrite the route
-    void updateRoute(); //Extraneous, this is done by setCurrentBeacon
+    void updateRoute();
     void printRoute();
     void reportLocation();
     void initializeMap();
     void updateMap();
     void printMap();
+    void collisionAvoidance(int otherCurrentBeacon, int otherNextBeacon, int otherDestination);
+    bool turnAround();
     int get_beacon_ID(int IR_count);
     int get_pin();
     
